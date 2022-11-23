bccc43fd9a2d125e659cc5adb70a7d32a74b7c5b
candidodimande <102628435+candidodimande@users.noreply.github.com>
2022-11-23 10:06:36 -0800

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,10 +5,14 @@ var line1 = document.querySelector('.line1')
 var line2 = document.querySelector('.line2')
 var line3 = document.querySelector('.line3')
 
+
+
+var menubtn = document.querySelector('#menubtn')
+
 menu_button.addEventListener('click', function(){
     menu.classList.toggle('show-menu')
-    menu_list.classList.toggle('show-menu-list')
+     menu_list.classList.toggle('show-menu-list')
     line1.classList.toggle('line1-rotation')
-    line2.classList.toggle('line2-opacity')
-    line3.classList.toggle('line3-rotation')
+     line2.classList.toggle('line2-opacity')
+     line3.classList.toggle('line3-rotation')
 });
